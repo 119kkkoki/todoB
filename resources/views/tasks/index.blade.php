4215508389d0d9f3a557228aee901542345b6b27
hikarinakanodayo <112676692+hikarinakanodayo@users.noreply.github.com>
2022-09-26 19:46:15 +0800

    一覧ページのUIを少々作りました。

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>一覧</title>
+    <link rel="stylesheet" href="{{asset('/asset/css/style.css') }}">
+</head>
+
+<body>
+  <header>
+    <nav>
+        <ul class="navbar">
+            <li><a href="">Create</a></li>
+            <li><a href="">Username</a></li>
+        </ul>
+    </nav>
+  </header>
+
+  <div class="content">
+
+      <div class="content-image">
+        <img src="写真" >
+      </div>
+
+      <div class="content-description">
+        <h2>タイトル</h2>
+        <p>中野は元気ですが何か？</p>
+        
+      </div>
+
+     
+        <svg class="bi" width="32" height="32" fill="currentColor">
+          <use xlink:href="bootstrap-icons.svg#heart-fill"/>
+        </svg>
+   
+      <form action="" method="post">
+        <button>EDIT</button>
+      </form>
+
+      <form action="" method="post">
+        <button>DELETE</button>
+      </form>
+
+  </div>
+</body>
+</html>
