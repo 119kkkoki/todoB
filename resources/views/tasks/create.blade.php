e41e1d77154ddd429b644328b02d2559b80d18c7
hikarinakanodayo <112676692+hikarinakanodayo@users.noreply.github.com>
2022-09-28 21:27:26 +0800

    viewファイルにheaderとfooterが継承される記述をしました

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,3 +1,5 @@
+@extends('layouts.app_original')
+@section('content')
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -21,3 +23,4 @@
         </div>
     </div>
   </div>
+  @endsection
--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/index', 'TaskController@index')->name('tasks.index');
-
 Route::get('/index/create', 'TaskController@create')->name('tasks.create');
 Route::post('/index', 'TaskController@store')->name('tasks.store');
 
